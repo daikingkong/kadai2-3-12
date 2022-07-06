dacfc63c0edea3a8235860a8405d00efa4874bcd
EC2 Default User <daikingkong>
2022-07-06 12:13:17 +0000

    [Add]3-12　indexのページ作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.save
+    redirect_to show_book_path(@book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -20,5 +23,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
