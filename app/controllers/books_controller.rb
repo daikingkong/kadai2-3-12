a1e754ffef722f18201d6862ec7cbfe9ae2ed212
EC2 Default User <daikingkong>
2022-07-08 08:13:30 +0000

    [Add]3-12 新規投稿と編集の際に空白時のエラーメッセージの表示機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render:index
+    end
   end
 
   def index
@@ -20,9 +25,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book_update = Book.find(params[:id])
+    if @book_update.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render:edit
+    end
   end
 
   def destroy
