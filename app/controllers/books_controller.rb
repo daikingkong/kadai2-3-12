733dafb85c5867e7bdf38b5e78fc651a7015fa76
EC2 Default User <daikingkong>
2022-07-08 09:01:44 +0000

    [Fix]3-12 Rspecで出てエラーの解消(エラーメッセージの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render:index
@@ -25,12 +25,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book_update = Book.find(params[:id])
-    if @book_update.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      @book = Book.find(params[:id])
       render:edit
     end
   end
